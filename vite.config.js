2f6973ae29f01481e8a059271d1dc87ad7fe5507
a.vinogradov <a.vinogradov@bpcbt.com>
2024-05-26 00:27:32 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,48 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import fs from 'fs';
+import path from 'path';
+import * as sass from 'sass';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
-    dts({ include: ['lib'] })
+    dts({ include: ['lib'] }),
+    {
+      name: 'prepare-styles',
+      apply: 'build',
+      enforce: 'post',
+      async writeBundle(_, bundle) {
+        // Compile SCSS
+        const result = await sass.compileAsync(path.resolve(__dirname, 'lib/index.scss'));
+        fs.writeFileSync(path.resolve(__dirname, 'dist/style.css'), result.css);
+
+        // Copy SCSS files to dist
+        const scssSrcDir = path.resolve(__dirname, 'lib');
+        const scssDestDir = path.resolve(__dirname, 'dist/scss');
+
+        // Ensure the destination directory exists
+        if (!fs.existsSync(scssDestDir)) {
+          fs.mkdirSync(scssDestDir, { recursive: true });
+        }
+
+        // Copy all SCSS files
+        const scssFiles = fs.readdirSync(scssSrcDir).filter((file) => file.endsWith('.scss'));
+
+        scssFiles.forEach((file) => {
+          const srcPath = path.join(scssSrcDir, file);
+          const destPath = path.join(scssDestDir, file);
+          fs.copyFileSync(srcPath, destPath);
+        });
+      },
+    },
   ],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
+      entry: path.resolve(__dirname, 'lib/index.ts'),
       name: 'CalendarLib',
       fileName: 'index',
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
+    rollupOptions: {},
   },
-})
+});
